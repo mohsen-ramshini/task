db9d5c7e8a8aa36f71ebf993c02ec1a67fc659f6
mohsen-ramshini <rsmohsen20@gmail.com>
2025-08-23 16:25:47 +0330

    feat: add routes for sign-in, sign-up , forget-password and dashboard pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import SignIn from "../features/auth/components/SigninForm";
+import SignUp from "../features/auth/components/SignupForm";
+import ForgetPassword from "../features/auth/components/ForgetPassword";
+import Dashboard from "../features/dashboard/components/Dashboard";
+import type { PropsWithChildren } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <div className=" text-black p-4 font">
-        اگر این متن پس‌زمینه آبی دارد، Tailwind فعال است.
-      </div>
-    </div>
-  );
+// Protected Route
+function ProtectedRoute({ children }: PropsWithChildren) {
+  const isAuth = !!localStorage.getItem("auth");
+  return isAuth ? children : <Navigate to="/signIn" />;
 }
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/sign-in" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
