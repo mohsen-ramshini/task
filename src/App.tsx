973768120d9b7a9c2db663338eb6f178c98ab5a4
mohsen-ramshini <rsmohsen20@gmail.com>
2025-08-23 15:30:48 +0330

    chore: initialize project with Vite + React + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className=" text-black p-4 font">
+        اگر این متن پس‌زمینه آبی دارد، Tailwind فعال است.
+      </div>
+    </div>
+  );
+}
+
+export default App;
